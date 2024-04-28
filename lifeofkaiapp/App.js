f853aa9dd189ed58c1af099c2b0cdb6f90b02e04
Ryan Riley Puzon <rrgpuzon@gmail.com>
2024-04-28 03:39:42 -0700

    asks for location permission

--- lifeofkaiapp/App.js
+++ lifeofkaiapp/App.js
@@ -21,15 +21,29 @@
 // });
 
 //imported open source
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, SafeAreaView, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./HomeScreen";
+import * as Location from 'expo-location';
 
 const Stack = createStackNavigator();
 
 const Launchpage = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const askForLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied'); return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+    askForLocation();
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
